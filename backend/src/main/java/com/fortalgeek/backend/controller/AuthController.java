3d2690c7783b5b0b54488f9040a2791ba4eb1b7c
Your Name <you@example.com>
2026-09-15 09:00:11 -0300

    feat: adiciona empresa e o fluxo de usuario

--- backend/src/main/java/com/fortalgeek/backend/controller/AuthController.java
+++ backend/src/main/java/com/fortalgeek/backend/controller/AuthController.java
@@ -1,13 +1,15 @@
 package com.fortalgeek.backend.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.RequestBody;
-import com.fortalgeek.backend.service.AuthService;
-import com.fortalgeek.backend.dto.LoginRequest;
-import com.fortalgeek.backend.dto.LoginResponse;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.fortalgeek.backend.dto.CadastroEmpresaRequest;
+import com.fortalgeek.backend.dto.LoginRequest;
+import com.fortalgeek.backend.dto.LoginResponse;
+import com.fortalgeek.backend.service.AuthService;
 
 @RestController
 @RequestMapping("/api/auth")
@@ -25,6 +27,16 @@ public class AuthController {
             return ResponseEntity.ok(response);
         } else {
             return ResponseEntity.status(401).body(response);
+        }
+    }
+    //FAZ A ROTA ASSIM PARA CADASTRO DE USUARIO COMUM
+    @PostMapping("/cadastro/empresa")
+    public ResponseEntity<String> cadastrarEmpresa(@RequestBody CadastroEmpresaRequest request) {
+        try {
+            authService.cadastrarEmpresa(request);
+            return ResponseEntity.status(201).body("Empresa cadastrada com sucesso!");
+        } catch (RuntimeException e) {
+            return ResponseEntity.status(400).body(e.getMessage());
         }
     }
 
